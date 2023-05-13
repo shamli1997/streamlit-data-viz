2452695f141474b72c695a20b64cb711832e49b7
Shamli Ingole <54771718+shamli1997@users.noreply.github.com>
2023-05-13 11:44:48 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 # Load the data
-data = pd.read_csv("your_data.csv")  # Replace "your_data.csv" with the actual file path or URL
+data = pd.read_csv("assign2_wastedata.csv")  # Replace "your_data.csv" with the actual file path or URL
 
 # Extract the year from the date column
 data["Year"] = pd.to_datetime(data["Date"]).dt.year
