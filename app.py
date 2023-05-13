a7a826a2f5982df3a94c7bd28f1b8c1f98ad8ff9
Shamli Ingole <54771718+shamli1997@users.noreply.github.com>
2023-05-13 11:50:30 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
-# import streamlit as st
-# import pandas as pd
-
-# # Load the data
-# data = pd.read_csv("assign2_wastedata.csv")  # Replace "your_data.csv" with the actual file path or URL
-
-# # Extract the year from the date column
-# data["Year"] = pd.to_datetime(data["Date"]).dt.year
-
-# # Calculate the total waste for each year
-# yearly_waste = data.groupby("Year")["Weight"].sum()
-
-# # Find the year with the highest waste
-# year_with_highest_waste = yearly_waste.idxmax()
-
-# # Display the result
-# st.write("Year with the highest waste:", year_with_highest_waste)
 import streamlit as st
 import pandas as pd
+
+# Apply custom CSS styles
+st.markdown(
+    """
+    <style>
+    body {
+        background-color: #222222;
+        color: #ffffff;
+    }
+    .stMetric {
+        background-color: #444444;
+        box-shadow: 2px 2px 5px rgba(255, 255, 255, 0.1);
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 # Read the CSV file
 data = pd.read_csv("assign2_wastedata.csv")
@@ -37,15 +37,18 @@ year_with_max_waste = yearly_waste.idxmax()
 total_waste_max_year = yearly_waste.max()
 
 # Create a Streamlit app
-st.title("Waste Visualization")
+st.title("Waste Dashboard")
 
-# Display the total waste in each year
-st.subheader("Total Waste Generated in Each Year")
-st.bar_chart(yearly_waste)
+# Create a layout with two columns
+col1, col2 = st.beta_columns(2)
 
-# Display the year with the highest waste and the total waste in that year
-st.subheader("Year with the Highest Waste")
-st.write("Year:", year_with_max_waste)
-st.write("Total Waste:", total_waste_max_year)
+# Upper portion - Tile with the year and amount of waste
+with col1:
+    st.subheader("Year with Most Waste")
+    st.metric("Year", year_with_max_waste)
+    st.metric("Total Waste (lbs)", total_waste_max_year)
 
-    
+# Lower portion - Visualization (e.g., bar chart)
+with col2:
+    st.subheader("Total Waste Generated in Each Year")
+    st.bar_chart(yearly_waste)
