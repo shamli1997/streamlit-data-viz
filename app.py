9a283357716626764e10558b76ce8c659110092c
Shamli Ingole <54771718+shamli1997@users.noreply.github.com>
2023-05-13 12:04:36 -0700

    Update app.py

--- app.py
+++ app.py
@@ -44,11 +44,21 @@ st.title("Waste Dashboard")
 
 # Upper portion - Tile with the year and amount of waste
 st.subheader("Year with Most Waste")
-with st.container():
-    st.markdown(
-        f'<div class="tile"><h2>{year_with_max_waste}</h2><h3>Total Waste (lbs)</h3><p>{total_waste_max_year}</p></div>',
-        unsafe_allow_html=True
-    )
+col1, col2, col3 = st.columns(3)
+
+with col1:
+   st.header("A cat")
+   st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+   st.header("A dog")
+   st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+   st.header("An owl")
+   st.image("https://static.streamlit.io/examples/owl.jpg")
+Copy
+
 
 # Lower portion - Visualization (e.g., bar chart)
 st.subheader("Total Waste Generated in Each Year")
