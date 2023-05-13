653b3152e8046fb90fb7d330b9255b2ed60a2984
Shamli Ingole <54771718+shamli1997@users.noreply.github.com>
2023-05-13 11:41:48 -0700

    Added new files

--- app.py
+++ app.py
@@ -1,33 +1,19 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 
-st.text('hello world')
+# Load the data
+data = pd.read_csv("your_data.csv")  # Replace "your_data.csv" with the actual file path or URL
 
+# Extract the year from the date column
+data["Year"] = pd.to_datetime(data["Date"]).dt.year
 
+# Calculate the total waste for each year
+yearly_waste = data.groupby("Year")["Weight"].sum()
 
-# Create sample data
-data = {
-    'x': [1, 2, 3, 4, 5],
-    'y': [2, 4, 6, 8, 10],
-    'category': ['A', 'A', 'B', 'B', 'B']
-}
-df = pd.DataFrame(data)
+# Find the year with the highest waste
+year_with_highest_waste = yearly_waste.idxmax()
 
-# Define the plot type selection widget
-plot_type = st.selectbox('Select plot type', ('Scatter plot', 'Bar graph'))
+# Display the result
+st.write("Year with the highest waste:", year_with_highest_waste)
 
-# Plot the selected type of plot
-if plot_type == 'Scatter plot':
-    fig, ax = plt.subplots()
-    ax.scatter(df['x'], df['y'])
-    ax.set_xlabel('X')
-    ax.set_ylabel('Y')
-    st.pyplot(fig)
-elif plot_type == 'Bar graph':
-    fig, ax = plt.subplots()
-    df.groupby('category')['y'].sum().plot(kind='bar', ax=ax)
-    ax.set_xlabel('Category')
-    ax.set_ylabel('Y')
-    st.pyplot(fig)
     
