cace2119e3de0e9669790bf636941e2ad47a74a0
Shamli Ingole <54771718+shamli1997@users.noreply.github.com>
2023-05-13 12:10:55 -0700

    Update app.py

--- app.py
+++ app.py
@@ -56,21 +56,6 @@ with st.beta_container():
 # Lower portion - Visualization (e.g., bar chart)
 st.subheader("Total Waste Generated in Each Year")
 st.bar_chart(yearly_waste)
-col1, col2, col3 = st.columns(3)
-
-with col1:
-   st.header("A cat")
-   st.image("https://static.streamlit.io/examples/cat.jpg")
-
-with col2:
-   st.header("A dog")
-   st.image("https://static.streamlit.io/examples/dog.jpg")
-
-with col3:
-   st.header("An owl")
-   st.image("https://static.streamlit.io/examples/owl.jpg")
-Copy
-
 
 # Lower portion - Visualization (e.g., bar chart)
 st.subheader("Total Waste Generated in Each Year")
